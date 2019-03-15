2f5d1002b6dfc4b141631575a1f5c9887a9a2f76
Alex <aleksgrin96@gmail.com>
2019-03-15 19:23:40 +0300

    подключены минифицированные файлы js и css, Исправлены мелкие ошибки по техническому заданию

--- js/device.js
+++ js/device.js
@@ -12,27 +12,27 @@ var email = formWrite.querySelector("input#email");
 console.log(mapBtn);
 
 writeBtn.addEventListener("click", function(event) {
-	event.preventDefault();
-	formWrite.classList.remove("modal-hide");
+    event.preventDefault();
+    formWrite.classList.remove("modal-hide");
     formWrite.classList.add("modal-show");
 });
 
 closeWrite.addEventListener("click", function(event) {
-	event.preventDefault();
-	formWrite.classList.remove("modal-show");
+    event.preventDefault();
+    formWrite.classList.remove("modal-show");
     formWrite.classList.add("modal-hide");
     formWrite.classList.remove("form-error");
 });
 
 mapBtn.addEventListener("click", function(event) {
-	event.preventDefault();
-	map.classList.remove("modal-hide");
+    event.preventDefault();
+    map.classList.remove("modal-hide");
     map.classList.add("modal-show");
 });
 
 closeMap.addEventListener("click", function(event) {
-	event.preventDefault();
-	map.classList.remove("modal-show");
+    event.preventDefault();
+    map.classList.remove("modal-show");
     map.classList.add("modal-hide");
 });
 
